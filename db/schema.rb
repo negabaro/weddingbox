422d73e40797ee696e206d5838f91a0eddb32e74
negabaro <negabaro@gmail.com>
2016-02-17 21:21:37 +0900

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211035718) do
+ActiveRecord::Schema.define(version: 20160217024928) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "context"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160211035718) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
